58d6660dc061af5f61322c82a367b1d61bb10e81
gn10g <8gn24gn25@gmail.com>
2026-01-02 23:56:03 +0900

    Add CANManager class and remove empty driver_interface.hpp file

---
+++ include/gn10_can/core/can_manager.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+#include "gn10_can/core/can_device.hpp"
+#include "gn10_can/drivers/driver_interface.hpp"
+
+namespace gn10_can {
+
+class CANManager {
+ public:
+};
+}  // namespace gn10_can
