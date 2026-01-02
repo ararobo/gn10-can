500f4cfc62e0fd722fa8431858cdebb3bc784c79
gn10g <8gn24gn25@gmail.com>
2026-01-02 12:52:40 +0900

    Add CANFrame structure to define CAN message format

---
+++ include/gn10_can_models/core/can_frame.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+namespace gn10_can
+{
+
+struct CANFrame
+{
+    uint32_t id = 0;
+    std::array<uint8_t, 8> data;
+    uint8_t len;
+};
+
+} // namespace gn10_can
