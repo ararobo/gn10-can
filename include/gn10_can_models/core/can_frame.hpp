6237b3c24f6d7648363df6c6c44618a695c3198b
gn10g <8gn24gn25@gmail.com>
2026-01-02 14:06:57 +0900

    Initialize data array and length in CANFrame structure

--- include/gn10_can_models/core/can_frame.hpp
+++ include/gn10_can_models/core/can_frame.hpp
@@ -7,8 +7,8 @@ namespace gn10_can {
 
 struct CANFrame {
     uint32_t id = 0;
-    std::array<uint8_t, 8> data;
-    uint8_t len;
+    std::array<uint8_t, 8> data{};
+    uint8_t len = 0;
 };
 
 }  // namespace gn10_can
