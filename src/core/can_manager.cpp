e0657a21448f4b5702a4f9f7eab3540b2225fac8
gn10g <8gn24gn25@gmail.com>
2026-01-03 00:47:53 +0900

    Refactor CANManager to add null check in resister_device and improve update method structure

--- src/core/can_manager.cpp
+++ src/core/can_manager.cpp
@@ -5,10 +5,24 @@
 namespace gn10_can {
 
 CANManager::CANManager(drivers::DriverInterface& driver) : driver_(driver) {}
-CANManager::~CANManager() {}
 
 void CANManager::resister_device(CANDevice* device) {
-    devices_.push_back(device);
+    if (device) {
+        devices_.push_back(device);
+    }
+}
+
+void CANManager::update() {
+    CANFrame rx_frame;
+    while (driver_.receive(rx_frame)) {
+        for (auto* device : devices_) {
+            device->on_receive(rx_frame);
+        }
+    }
+}
+
+bool CANManager::send_frame(const CANFrame& frame) {
+    return driver_.send(frame);
 }
 
 }  // namespace gn10_can
