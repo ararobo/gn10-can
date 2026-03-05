ec1aa120c01af59a209322c0eda15b10ab1b8d61
Gento Aiba <130132820+aiba-gento@users.noreply.github.com>
2026-03-05 20:39:43 +0900

    fix:spelling mistake

--- src/devices/servo_moter_client.cpp
+++ src/devices/servo_moter_client.cpp
@@ -5,24 +5,24 @@
 namespace gn10_can {
 namespace devices {
 
-ServoMoterClient::ServoMoterClient(CANBus& bus, uint8_t dev_id)
-    : CANDevice(bus, id::DeviceType::ServoMoter, dev_id)
+ServoMotorClient::ServoMotorClient(CANBus& bus, uint8_t dev_id)
+    : CANDevice(bus, id::DeviceType::ServoMotor, dev_id)
 {
 }
 
-void ServoMoterClient::set_init(uint16_t min_us, u_int16_t max_us)
+void ServoMotorClient::set_init(uint16_t min_us, u_int16_t max_us)
 {
     std::array<uint8_t, 4> payload{};
     converter::pack(payload, 0, min_us);
     converter::pack(payload, 2, max_us);
-    send(id::MsgTypeServoMoter::Init, payload);
+    send(id::MsgTypeServoMotor::Init, payload);
 }
 
-void ServoMoterClient::set_angle_rad(float angle_rad)
+void ServoMotorClient::set_angle_rad(float angle_rad)
 {
     std::array<uint8_t, 4> payload{};
     converter::pack(payload, 0, angle_rad);
-    send(id::MsgTypeServoMoter::AngleRad, payload);
+    send(id::MsgTypeServoMotor::AngleRad, payload);
 }
 
 }  // namespace devices
