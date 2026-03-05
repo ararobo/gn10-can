acd47a1f9fdb3ee748d7677300d30c4aba7b36af
gn10 <8gn24gn25@gmail.com>
2026-03-05 21:33:24 +0900

    fix:純粋仮想関数をオーバーライド歯忘れていた

--- include/gn10_can/devices/servo_motor_client.hpp
+++ include/gn10_can/devices/servo_motor_client.hpp
@@ -14,6 +14,13 @@ public:
     void set_init(uint16_t min_us, uint16_t max_us);
 
     void set_angle_rad(float angle_rad);
+
+    /**
+     * @brief CANパケット受信時の呼び出し関数の実装
+     *
+     * @param frame 受信したCANパケット
+     */
+    void on_receive(const CANFrame& frame) override;
 };
 }  // namespace devices
 }  // namespace gn10_can
