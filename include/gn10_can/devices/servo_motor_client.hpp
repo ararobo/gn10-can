161ffc09c1386a95ed0460df0b9e55554b297e84
Kamagata0 <aroe0725@gmail.com>
2026-03-07 14:22:21 +0900

    feat:サーボモータの関数にDoxygen追加した

--- include/gn10_can/devices/servo_motor_client.hpp
+++ include/gn10_can/devices/servo_motor_client.hpp
@@ -1,3 +1,13 @@
+/**
+ * @file servo_motor_client.hpp
+ * @author Akeru Kamagata
+ * @brief サーボモータのクライアントのヘッダーファイル。
+ * @version 0.1
+ * @date 2026-03-07
+ *
+ * @copyright Copyright (c) 2026 ararobo
+ *
+ */
 #pragma once
 
 #include "gn10_can/core/can_bus.hpp"
@@ -10,9 +20,18 @@ class ServoMotorClient : public CANDevice
 {
 public:
     ServoMotorClient(CANBus& bus, uint8_t device_id);
-
+    /**
+     * @brief サーボモータのduty最大値と最小値の設定
+     *
+     * @param min_us　duty最大値
+     * @param max_us　duty最小値
+     */
     void set_init(uint16_t min_us, uint16_t max_us);
-
+    /**
+     * @brief　サーボモータで指定したい角度の設定
+     *
+     * @param angle_rad サーボモータの角度
+     */
     void set_angle_rad(float angle_rad);
 };
 }  // namespace devices
--- include/gn10_can/devices/servo_motor_server.hpp
+++ include/gn10_can/devices/servo_motor_server.hpp
@@ -12,8 +12,22 @@ class ServoMotorServer : public CANDevice
 {
 public:
     ServoMotorServer(CANBus& bus, uint8_t device_id);
-
+    /**
+     * @brief 受け取ったdutyの最大値と最小値の設定
+     *
+     * @param min_us　最大のduty値
+     * @param max_us　最小のduty値
+     * @return true
+     * @return false
+     */
     bool get_new_init(uint16_t& min_us, uint16_t& max_us);
+    /**
+     * @brief 受け取ったサーボモータの角度の設定
+     *
+     * @param angle_rad　サーボモータの角度
+     * @return true
+     * @return false
+     */
     bool get_new_angle_rad(float& angle_rad);
     void on_receive(const CANFrame& frame) override;
 
