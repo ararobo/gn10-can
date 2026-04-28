c9b3726c7378776140919c9bb6eed55335974b5f
aiba-gento <8gn24gn25@gmail.com>
2026-04-29 00:20:58 +0900

    feat:電源管理デバイスのクライアントAPI作成

---
+++ include/gn10_can/devices/power_manager_client.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <optional>
+
+#include "gn10_can/core/fdcan_device.hpp"
+#include "gn10_can/utils/can_converter.hpp"
+
+namespace gn10_can {
+namespace devices {
+
+class PowerManagerClient : public FDCANDevice
+{
+public:
+    PowerManagerClient(FDCANBus& bus, uint8_t dev_id);
+
+    void set_init();
+
+    void set_stop(bool enable_stop);
+
+    bool get_feedback(bool enable_stop, float voltage, float current);
+
+    void on_receive(const FDCANFrame& frame) override;
+
+private:
+};
+}  // namespace devices
+}  // namespace gn10_can
