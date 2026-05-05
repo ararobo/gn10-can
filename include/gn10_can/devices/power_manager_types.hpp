18259f0ebc54a1ec91e3f5f7f3ea7e88f14a7c5e
aiba-gento <8gn24gn25@gmail.com>
2026-05-05 19:46:16 +0900

    fix:typedefは反映されないので消去

--- include/gn10_can/devices/power_manager_types.hpp
+++ include/gn10_can/devices/power_manager_types.hpp
@@ -5,19 +5,19 @@ namespace gn10_can {
 namespace devices {
 
 namespace power_manager {
-typedef struct Config {
+struct Config {
     bool use_remote_emergency_stop = false;
     uint16_t sensor_rate_ms        = 1000;
 };
 
-typedef struct Status {
+struct Status {
     bool emergency_stop_enabled;
     bool remote_emergency_stop_connected;
     bool remote_emergency_stop_enabled;
     bool over_current;
 };
 
-typedef struct Sensor {
+struct Sensor {
     float voltage;
     float current;
 };
