7ae0042cff32788220eb827681ce8fbeac2bff7c
kanai0914 <ayuayugonbe@gmail.com>
2026-08-29 09:19:44 +0900

    docx:servet側に説明追加

--- include/gn10_can/devices/led_control_server.hpp
+++ include/gn10_can/devices/led_control_server.hpp
@@ -25,22 +25,67 @@ public:
     bool get_new_init(RGB& rgb);
 
     /**
-     * @brief 細かいLEDの設定を行い、それにIDを結びつける関数
+     * @brief 設定されたLEDIDを指定された範囲と共に受けとる関数
      * @param led_setting LEDの範囲・テープ番号設定
      * @param led_id LEDの識別ID
+     *
+     * @return true IDと紐付けられた範囲を取得することができた
+     * @return false IDと紐付けられた範囲を取得することができなかった
      */
     bool get_pixel_id(const LEDScopeSetting& led_setting, uint8_t led_id);
 
+    /**
+     * @brief LEDの識別番号設定を開放司令を取得する関数
+     * @param led_id 開放したいLEDの識別番号
+     *
+     * @return true 指定した範囲とLEDIDを開放することができた
+     * @return false  指定した範囲とLEDIDを開放することができた
+     */
     bool reset_pixels_id(uint8_t led_id);
 
+    /**
+     * @brief LEDの色と光らせ方を取得する関数（範囲：指定LEDID)
+     *
+     * @param led_id 設定されたLEDの識別ID
+     * @param rgb_config 色の設定
+     * @param show_type 光らせ方の指定
+     *
+     * @return true led_idに対して光らせる色を取得することができた
+     * @return false led_idに対して光らせる色を取得することができた
+     */
     bool get_color_type_led_id(uint8_t led_id, const RGB& rgb, ShowTypeLED show_type);
 
+    /**
+     * @brief LEDの色と光らせ方を取得する関数（範囲：LEDテープ)
+     *
+     * @param led_tape LEDtape番号
+     * @param rgb_config 色の設定
+     * @param show_type 光らせ方の指定
+     *
+     * @return true led_tapeに対して光らせる色を取得することができた
+     * @return false led_tapeに対して光らせる色を取得することができた
+     */
     bool get_color_type_led_tape(uint8_t led_tape, const RGB& rgb, ShowTypeLED show_type);
 
+    /**
+     * @brief LEDの色と光らせ方を取得する関数（範囲：すべてのLED)
+     *
+     * @param rgb_config 色の設定
+     * @param show_type 光らせ方の指定
+     *
+     * @return true すべてのLEDに対して光らせる色を取得することができた
+     * @return false すべてのLEDに対して光らせる色を取得することができた
+     */
     bool get_color_type_led_all(const RGB& rgb, ShowTypeLED show_type);
 
+    /**
+     * @brief LEDの点灯支持を受信する関数
+     */
     bool get_show_all();
 
+    /**
+     * @brief LEDの消灯支持を受信する関数
+     */
     bool get_clear_all();
 
     /**
