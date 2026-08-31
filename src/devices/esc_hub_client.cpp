b72b00c18b924ee501fb449eb1e577d8e842b9db
aiba-gento <8gn24gn25@gmail.com>
2026-08-31 11:44:47 +0900

    fix:unpack関数の統一

--- src/devices/esc_hub_client.cpp
+++ src/devices/esc_hub_client.cpp
@@ -62,7 +62,7 @@ void ESCHubClient::on_receive(const FDCANFrame& frame)
     if (id_fields.is_command(id::MsgTypeESCHub::Feedbacks)) {
         if (frame.dlc != sizeof(Feedbacks)) return;
         Feedbacks feedbacks;
-        if (converter::unpack(frame.data.data(), frame.dlc, 0, feedbacks)) {
+        if (converter::unpack(frame.data, 0, feedbacks)) {
             feedbacks_ = feedbacks;
         }
     }
