fb0090e970238ba00494255afc7a27e80bd5b7ba
aiba-gento <8gn24gn25@gmail.com>
2026-09-01 22:36:45 +0900

    fix:data lengthをdlcに修正

--- include/gn10_can/devices/robot_control_hub_server.hpp
+++ include/gn10_can/devices/robot_control_hub_server.hpp
@@ -57,7 +57,7 @@ public:
         if (id_fields.is_command(id::MsgTypeRobotControlHub::Command)) {
             if (frame.dlc != dlc::data_length_to_dlc(sizeof(Command))) return;
             Command command;
-            if (converter::unpack(frame.data.data(), frame.data_length, 0, command)) {
+            if (converter::unpack(frame.data.data(), frame.dlc, 0, command)) {
                 command_ = command;
             }
         }
